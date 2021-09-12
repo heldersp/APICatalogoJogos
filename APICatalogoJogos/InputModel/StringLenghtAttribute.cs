e49fe33284c159f4a5d3e4d753bbaed99f46e357
Helder da Silva Galbier <heldergalbier@gmail.com>
2021-09-12 20:21:11 -0300

    Adicionar arquivos de projeto.

---
+++ APICatalogoJogos/InputModel/StringLenghtAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace APICatalogoJogos.InputModel
+{
+    internal class StringLenghtAttribute : Attribute
+    {
+        private int v;
+        private int minimumLenght;
+        private string errorMessage;
+
+        public StringLenghtAttribute(int v, int MinimumLenght, string ErrorMessage)
+        {
+            this.v = v;
+            minimumLenght = MinimumLenght;
+            errorMessage = ErrorMessage;
+        }
+
+        public int MinimumLenght { get; set; }
+    }
+}
